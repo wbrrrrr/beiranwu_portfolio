e41a09fa51a9098b384e3fd2635debc7cf13313a
wbrrrrr <108027920+wbrrrrr@users.noreply.github.com>
2026-09-18 17:31:23 -0400

    Update portfolio website link

--- App.tsx
+++ App.tsx
@@ -2998,7 +2998,7 @@ function ResumePage() {
                   <line x1="2" y1="12" x2="22" y2="12" />
                   <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1 4-10z" />
                 </svg>
-                wubeiran.framer.website
+                https://wbrrrrr.github.io/beiranwu_portfolio/
               </span>
             </div>
           </div>
